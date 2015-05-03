97b443d415ca70aefd8a802e13e2bd1e0f61403a
Tiago Canassa Bernardinelli <tiago.bernardinelli@gmail.com>
2015-05-03 15:13:37 -0300

    Erro da Rede

--- src/Holdout.java
+++ src/Holdout.java
@@ -4,6 +4,12 @@ public class Holdout implements EstrategiaTeste{
 	
 	private List<Exemplo> subListaTeste = null;
 	private List<Exemplo> subListaExercicio = null;
+	private RNA rna;
+	
+	public Holdout(RNA rna){
+		this.rna = rna;
+	}
+	
 	
 	public void divideMassa(List<Exemplo> exemplos){
 		int size = tuples.size() / 3;
@@ -13,9 +19,24 @@ public class Holdout implements EstrategiaTeste{
 	
 	public double epoca(){
 		for (Exemplo e: subListaExercicio){
-			
+			rna.treinarRede(e);
 		}
-		
+		double erroTotalRede = 0d;
+		for (Exemplo e: subListaTeste){
+			double erroExemplo = 0d;
+			
+			double[] resultadoObtido = rna.executar(e);
+			
+			Double classe = e.getClasse();
+			Double[] resultadoEsperado = new Double[resultadoObtido.length];
+			Arrays.fill(resultadoEsperado, 0d);
+			resultadoEsperado[classe.intValue] = 1.0d;	
+			
+			for (int i = 0; i < resultadoObtido.length; i++){
+				erroExemplo += Math.pow(resultadoEsperado[i] - resultadoObtido[i], 2);
+			}
+			erroTotalRede += erroExemplo;	
+		}
 		return 0.0d;
 	}
 }
